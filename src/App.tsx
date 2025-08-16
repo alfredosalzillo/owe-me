22261b20420a3a8d7f6f8de89ddd4c3e942cfb08
a.salzillo <a.salzillo@factor-eleven.com>
2025-08-16 17:49:26 +0200

    chore: removed useless wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import "./globals.css";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { DialogsProvider, NotificationsProvider } from "@toolpad/core";
 import React, { lazy } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router";
-import AppProviders from "@/components/AppProviders";
 import theme from "@/theme";
-import "./globals.css";
 
 const Home = lazy(() => import("@/routes/Home"));
 const GroupPage = lazy(() => import("@/routes/GroupPage"));
@@ -12,15 +12,16 @@ const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppProviders>
-        <Router>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="/groups/:id" element={<GroupPage />} />
-          </Routes>
-        </Router>
-        <CssBaseline />
-      </AppProviders>
+      <DialogsProvider>
+        <NotificationsProvider>
+          <Router>
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="/groups/:id" element={<GroupPage />} />
+            </Routes>
+          </Router>
+        </NotificationsProvider>
+      </DialogsProvider>
     </ThemeProvider>
   );
 };
