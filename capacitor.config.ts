eadae017f6f12d3a2e248cc872895c301822bace
a.salzillo <a.salzillo@factor-eleven.com>
2025-08-16 17:00:47 +0200

    chore: added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.oweme.app',
+  appName: 'owe-me',
+  webDir: 'out'
+};
+
+export default config;
