c3b7b5e5c7afec9cddbe3e61bfe731fa5f913420
a.salzillo <a.salzillo@factor-eleven.com>
2025-08-18 16:27:26 +0200

    feat(vite-plugin-react-router): hot reload when routes change

--- src/plugins/vite-plugin-react-app-router/index.ts
+++ src/plugins/vite-plugin-react-app-router/index.ts
@@ -1,3 +1,4 @@
+import deepEqual from "deep-equal";
 import { Plugin } from "vite";
 import { createRouterFileCode, parse } from "./route-tree";
 
@@ -9,6 +10,7 @@ const vitePluginReactAppRouter = (
   config: VitePluginReactAppRouterConfig,
 ): Plugin => {
   const root = config.root ?? "src";
+  let tree = parse(root);
   return {
     name: "vite-plugin-react-app-router",
     resolveId(id) {
@@ -21,8 +23,21 @@ const vitePluginReactAppRouter = (
       if (!id.includes(routerModuleId)) {
         return;
       }
-      const tree = parse(root);
+      tree = parse(root);
       return createRouterFileCode("/src", tree);
+    },
+    handleHotUpdate: (context) => {
+      const newTree = parse(root);
+      if (deepEqual(tree, newTree, { strict: true })) {
+        return;
+      }
+      const module = context.server.moduleGraph.getModuleById(
+        `${routerModuleId}.js`,
+      );
+      if (!module) {
+        return;
+      }
+      return [context.modules].flat().concat(module);
     },
   };
 };
