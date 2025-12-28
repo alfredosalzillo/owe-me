afc940ec8b33f42e9c8c088bea0a41d0e0c04aca
a.salzillo <a.salzillo@factor-eleven.com>
2025-12-28 10:52:35 +0000

    refactor: simplify imports in InviteMemberDialog component

--- src/components/InviteMemberDialog.tsx
+++ src/components/InviteMemberDialog.tsx
@@ -10,8 +10,7 @@ import {
   TextField,
 } from "@mui/material";
 import Box from "@mui/material/Box";
-import { DialogProps } from "@toolpad/core";
-import { useNotifications } from "@toolpad/core/useNotifications";
+import { DialogProps, useNotifications } from "@toolpad/core";
 import React, { useState } from "react";
 import { createInvite } from "@/plugins/api/invites";
 
